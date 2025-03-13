f6707547ca20461129ea56ad7ae05cc5bd225c96
nagdista <nagdista@gmail.com>
2025-03-13 08:48:04 +0200

    add icon to open and close

--- src/components/websiteDesign/WebsiteDesign.tsx
+++ src/components/websiteDesign/WebsiteDesign.tsx
@@ -1,28 +1,33 @@
+import { ChevronDown } from "lucide-react";
+import { useState } from "react";
 import Card from "./Card";
 import { websiteDesingData } from "./data";
 
-interface Iprops {}
+const WebsiteDesign = () => {
+  const [isOpen, setIsOpen] = useState(true);
 
-const WebsiteDesign = ({}: Iprops) => {
+  const openClose = () => {
+    setIsOpen(prevState => !prevState);
+  };
+
   return (
-    <div className= "website-design-container bg-blue-500 rounded-[5px] h-[280px] overflow-y-scroll relative mb-[30px] scroll-smooth">
-  
-    
-      <div className="bg-blue-500 z-10 text-white text-[20px] sticky top-0 left-0 font-[700]">
-        <h1 className=" py-2 px-4 w-full rounded-2xl ">
-          Website Design
-        </h1>
+    <div className="website-design-container bg-blue-500 rounded-[5px] max-h-[280px] overflow-y-scroll relative mb-[30px] scroll-smooth">
+      <div className="bg-blue-500 z-10 text-white text-[20px] sticky top-0 left-0 font-[700] flex justify-between items-center">
+        <h1 className="py-2 px-4 w-full rounded-2xl">Website Design</h1>
+        <ChevronDown
+          className="open-close mr-[10px] cursor-pointer"
+          onClick={openClose}
+          style={{ transform: isOpen ? 'rotate(0deg)' : 'rotate(270deg)' }}
+        />
       </div>
-  
-      {/* المحتوى المتبقي */}
-      <div className="cards flex justify-between flex-wrap mt-[20px] gap-3  px-[10px] pb-[30px]">
-     
-        {
-            websiteDesingData.map((e)=>{
-                return( <Card lecName={e.lecName} lecLink={e.lecLink} />)
-            })
-        }
-      </div>
+
+      {isOpen && (
+        <div className="cards flex justify-between flex-wrap mt-[20px] gap-3 px-[10px] pb-[30px]">
+          {websiteDesingData.map((e) => {
+            return <Card lecName={e.lecName} lecLink={e.lecLink} />;
+          })}
+        </div>
+      )}
     </div>
   );
 };
